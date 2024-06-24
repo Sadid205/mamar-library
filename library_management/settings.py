a3d221f2c8ee8306112855b85abbe9552549b0c2
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-06-24 22:18:22 +0600

    changed

--- library_management/settings.py
+++ library_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://mamar-library-paiy.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-library-mfn0.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
